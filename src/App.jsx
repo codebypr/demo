fa1455040b1cdc2f3f642ffc5279ca2f30f3a7ef
unknown <panchalpritam105@gmail.com>
2024-02-19 13:45:58 +0430

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Client, Databases, ID } from 'appwrite';
 
 function App() {
-  const [count, setCount] = useState(0)
+ let client;
+  const [uname, setUname] = useState();
+  const [password, setPassword] = useState();
+  const [arr,setArr]=useState([]);
+  const setValue=()=>{
+    client=new Client();
+    client.setEndpoint('https://cloud.appwrite.io/v1').setProject('65d04ecb366dcd6e13af');
+    const database=new Databases(client);
+    const data=database.createDocument('65d04ff6c49e72c2e1f9','101',ID.unique(),{'uname':uname,'password':password});
 
+    data.then((res)=>console.log(res.uname,'Done!')).catch((err)=>console.log(err,'Err'))
+  }
+
+  const getValue=async()=>{
+    client=new Client();
+    client.setEndpoint('https://cloud.appwrite.io/v1').setProject('65d04ecb366dcd6e13af');
+    const database=new Databases(client);
+    let data=database.listDocuments('65d04ff6c49e72c2e1f9','101');
+    await  data.then  ((res)=>setArr(res.documents)).catch((err)=>console.log(err,'Err'))
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>HELLO WORLD</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div style={{display:'flex'}}> 
+       <div style={{padding:'4px' ,border:'1px solid black'}}>
+          Username: <input type="text" style={{marginBottom:'4px'}}
+          value={uname}
+          onChange={(e)=>setUname(e.target.value)}
+          /><br />
+          Password: <input type="password" style={{marginBottom:'4px'}}
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+          /><br />
+          <button style={{border:"2px solid black ",width:'50%' ,backgroundColor:'greenyellow'}}
+          onClick={setValue}
+          > ADD</button>
+          <button style={{border:"2px solid black ",width:'50%' ,backgroundColor:'greenyellow'}}
+          onClick={getValue}
+          > View</button>
+        </div>
+
+        <table >
+          {arr.map((val)=>(<tr style={{border:'1px solid black'}}><td style={{border:'1px solid black'}}>{val.uname}</td ><td style={{border:'1px solid black'}}>{val.password}</td></tr>))}
+        </table>
+       </div>
     </>
   )
 }
